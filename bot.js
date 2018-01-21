fb2612ef219f3cf7d0668fd247aacfba083146e9
Aree-Vanier <gregory@ajayinkingston.com>
2018-01-21 16:39:32 -0500

    Setup primitive functionality

--- bot.js
+++ bot.js
@@ -6,9 +6,18 @@ client.on('ready', () => {
   console.log("Online");
 });
 
+var prefix = "!VOTE "
 client.on('message', message => {
-  if(message.content === "ping"){
-    message.reply("pong");
+  if(message.content.startsWith(prefix) $$ message.author != client.user){
+    if(message.content === prefix+"ping"){
+      message.channel.send("pong");
+    }
+    else if(message.content === prefix+"help"){
+      message.channel.send("ping:\n\treturns pong")
+    }
+    else{
+      message.channel.send("Unknown Command. Say !VOTE help for help")
+    }
   }
 });
 
