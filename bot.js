68a11e6a16d01e9e47b4d9fab710ce51ddc90dc9
Aree-Vanier <gregory@ajayinkingston.com>
2018-01-21 16:02:27 -0500

    Gitignored settings.json (contains token) added login to bot.js and ping/pong message

--- bot.js
+++ bot.js
@@ -1,6 +1,16 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
+const settings = require('./settings.json');
 
 client.on('ready', () => {
   console.log("Online");
 });
+
+client.on('message', message => {
+  if(message.content === "ping"){
+    message.reply("pong");
+  }
+});
+
+
+client.login(settings.token);
