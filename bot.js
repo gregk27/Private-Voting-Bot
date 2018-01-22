a6ecbdaa812cc8cd73b37109f401ef1caf5431d2
Aree-Vanier <gregory@ajayinkingston.com>
2018-01-21 20:33:54 -0500

    Start command arguments working

--- bot.js
+++ bot.js
@@ -15,10 +15,10 @@ client.on('message', message => {
     var valid = false;
     for(var i = 0; i < commands.length; i++){
       var command = commands[i];
-      if(content == command.trigger){
+      if(content.split(" ")[0] == command.trigger){
         valid = true;
-        args = content.split(" ");
-        args.shift();
+        args = content.substring(content.split(" ")[0].length+1);
+        console.log(args);
         call(command, message, args);
       }
     }
@@ -30,6 +30,45 @@ client.on('message', message => {
 
 function call(command, message, args){
   eval(command.function);
+}
+
+function start(msgData, args){
+  var ID = msgData.author.tag.split("#")[1] + " " + args.split(" ")[0];
+  var options = [];
+  var split = args.split("\"");
+  for(var i = 1; i < split.length; i++){
+    if(split[i+1] == ","||split[i-1] == ","){
+      options.push(split[i]);
+    }
+    else if(split[i] == ","){
+      continue;
+    }
+    else{
+      break;
+    }
+  }
+
+  var mode = "FPTP";
+  var target = 0;
+  var privacy = false;
+  if(args.includes("STV")){
+    mode = "STV"
+    var target= args[args.indexOf("STV")+4]+args[args.indexOf("STV")+5];
+    target = target.replace(" ", "");
+  }
+  if(args.includes("-p")||args.includes("--private")){
+    privacy = true;
+  }
+
+
+
+  console.log("NEW POLL");
+  console.log("\t" + ID);
+  console.log("\t" + options);
+  console.log("\t" + mode);
+  console.log("\t" + target);
+  console.log("\t" + privacy);
+  send(msgData.author+" Started a vote", msgData);
 }
 
 function send(message, msgData){
